1419acbe4c242ab58e092a64734883d6fea4c9cb
Phiphat050647 <112453645+Phiphat050647@users.noreply.github.com>
2022-11-27 17:01:11 +0700

    เพิ่มชุดคำสังออเดอร์#ต้องเขียนลดจำนวน stock

--- Script/main.py
+++ Script/main.py
@@ -12,7 +12,7 @@ def showmenu() : #Display Interface Menu (Funtction)
 def inputmenu() : # Enter word hook showmenu
 
     menu_check = True # check menu list
-    while menu_check : # if true is work loop while
+    while menu_check : # if true is work loop whiles
 
         menu = input(str("showmenu : ")).upper()
 
@@ -32,27 +32,58 @@ def inputmenu() : # Enter word hook showmenu
 
 
 # # #First Function  runtime # # #
-
-          
+               
 def shopping() : 
     with open(r"Datapacks\datalist.txt") as file:
         resource = file.read().splitlines()
     
-    print("-"*50)
-    print("%-2s %s %-10s %-19s %-2s %s %2s"% ("|","Code","|","Food Menu","|","Price","|"))
-    print("-"*50)
+    print("-"*55)
+    print("%-2s %s %-15s %-19s %-2s %s %2s"% ("|","Code","|","Food Menu","|","Price","|"))
+    print("-"*55)
 
     for i in range(len(resource)) : # ดึงเอา txt resource splitlines() 
         
         resource_conlist = resource[i] # แปลง text เป็น List 
-        resource_conlist = list(resource_conlist.split(" ")) # แปลง text เป็น List 
+        resource_conlist = list(resource_conlist.split(",")) # แปลง text เป็น List 
 
-        print("%-3s %-3s %-12s %-17s %-3s %-4s %2s"% ("|",resource_conlist[0],"|",resource_conlist[1],"|",resource_conlist[2],"|"))
-        print("-"*50)
+        print("%-3s %-3s %-7s %-27s %-3s %-4s %2s"% ("|",resource_conlist[0],"|",resource_conlist[1],"|",resource_conlist[2],"|"))
+        print("-"*55)
+    
+    order()
+    
+def order() : #ฟังชันก์สั่งออเดอร์ พร้อมบอกจำนวน
+    with open(r"Datapacks\datalist.txt") as file:
+        resource = file.read().splitlines()
+        
+    oder_check = True 
+    while oder_check :
+        
+        oder = int(input("Please enter your order : "))
+        amt = int(input("Please enter the required quantity : "))
+        
+    for i in range(len(resource)) : # ดึงเอา txt resource splitlines() 
+        
+        resource_conlist = resource[i] # แปลง text เป็น List 
+        resource_conlist = list(resource_conlist.split(",")) # แปลง text เป็น List 
+        
+        if oder == resource_conlist[0] :
+            total = resource_conlist[3] - amt
+            resource
+            #inplace_change("Datapacks\datalist.txt", resource_conlist[3] , total) เขียนแทนทีไฟล์
+            #เดี๋ยวมาเขียนต่อ
+            
+
+def stock_change(filename, old_int, new_int):
+    with open(filename) as f:
+        s = f.read()
+        if old_int not in s:
+            
+        
+           
+            
+        
 
 
-
-#def order() :
         
 #def stock() :
 
